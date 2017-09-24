c1f0eee1fa1420ed78cecd4b77fb09b15d5ebd91
Jimmy <jimmytsoares@gmail.com>
2017-09-24 16:31:25 -0300

    -

--- analogReadWriteSerial/analogReadWriteSerial.ino
+++ analogReadWriteSerial/analogReadWriteSerial.ino
@@ -4,7 +4,7 @@
  * 'fora' no 5V e no GND. O pino do meio no A0.
  * O potênciometro é um divisor de tensão que
  * irá variar o valor analógico lido em A0, 
- * variando a intensidade do LED da placa por
+ * variando a intensidade do LED por
  * PWM.
  */
 float x;
@@ -19,8 +19,11 @@ void loop() {
   x = analogRead(A0); //x recebe o valor analógico lido em A0
   x = x/1023; //divide por 1023 para ter valor de 0 a 1
   y = x*255; //multiplica por 255 para o PWM (que aceita de 0 a 255)
-  Serial.print(x*100); //imprime x
+  Serial.print("Entrada de ");
+  Serial.print(x*5); //imprime tensão entrada
+  Serial.print(" gera ");
+  Serial.print(x*100); //imprime % do duty cycle
   Serial.print("% Duty Cycle = analogWrite ");
   Serial.println(y); //imprime y e pula linha
-  analogWrite(LED_BUILTIN, y); //saída PWM no pino 13
+  analogWrite(ledPin, y); //saída PWM no pino 13
 }
--- btSoftwareSerial/btSoftwareSerial.ino
+++ btSoftwareSerial/btSoftwareSerial.ino
@@ -21,25 +21,29 @@
 
 SoftwareSerial myBt(10, 11); // RX, TX
 char BluetoothData;
+String fullData = "";
 int ledpin =  9;
 
 void setup() {
-  myBt.begin(9600);
+  myBt.begin(9600); //38400 para o BT7
   pinMode(ledpin,OUTPUT); //ledpin como saída
 }
 
 void loop() {
    if (myBt.available()){ //se tiver algo para ler
+      while(myBt.available()){
       BluetoothData=myBt.read(); //le e guarda
-      if(BluetoothData=='1'){   
+      fullData += BluetoothData;
+      }
+      if(fullData=="Led on"){   
           digitalWrite(ledpin,1);
           myBt.println("LED on!"); // vai imprimir no terminal do app
       }
-      else if (BluetoothData=='0'){
+      else if (fullData=="Led off"){
           digitalWrite(ledpin,0);
           myBt.println("LED off!");
       }
-
+      fullData = "";
   }
 delay(100);
 }
