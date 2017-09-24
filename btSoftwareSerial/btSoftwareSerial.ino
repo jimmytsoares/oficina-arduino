6599f327af227ca28f7c3a55d21d6b0913ee0232
Jimmy <jimmytsoares@gmail.com>
2017-09-23 23:02:51 -0300

    -

--- btSoftwareSerial/btSoftwareSerial.ino
+++ btSoftwareSerial/btSoftwareSerial.ino
@@ -20,19 +20,17 @@
 #include <SoftwareSerial.h>
 
 SoftwareSerial myBt(10, 11); // RX, TX
-int ledpin=5;
-char BluetoothData; 
+char BluetoothData;
+int ledpin =  9;
 
 void setup() {
   myBt.begin(9600);
-  Serial.begin(9600);
   pinMode(ledpin,OUTPUT); //ledpin como saída
-  pinMode(LED_BUILTIN, OUTPUT); //pino 13 como saída
 }
 
 void loop() {
    if (myBt.available()){ //se tiver algo para ler
-          BluetoothData=myBt.read(); //le e guarda
+      BluetoothData=myBt.read(); //le e guarda
       if(BluetoothData=='1'){   
           digitalWrite(ledpin,1);
           myBt.println("LED on!"); // vai imprimir no terminal do app
@@ -41,17 +39,7 @@ void loop() {
           digitalWrite(ledpin,0);
           myBt.println("LED off!");
       }
-      else if (BluetoothData=='3'){
-          digitalWrite(LED_BUILTIN,1);
-          myBt.println("Board LED on!");
-      }
-      else if (BluetoothData=='4'){
-          digitalWrite(LED_BUILTIN,0);
-          myBt.println("Board LED off!");
-      }
-      else {
-          myBt.println(BluetoothData);
-      }
+
   }
 delay(100);
 }
